5abd3ea58aefe330c3d7ea5ab826621906baab72
james <james.kopkiewicz@gmail.com>
2019-05-12 18:29:45 +0200

    wyrzucenie $data jako pole prywatne

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -5,13 +5,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 include(APPPATH . 'include/ApiConnectorPl.php');
 
 class Api extends CI_Controller {
-
-    public function __construct() {
-        parent::__construct();
-        $this->load->model('Tracking');
-        $this->load->model('Log');
-    }
-
+    
+    
+    private $data;
     private $descriptionLibrary = array(
         'Zarejestrowano dane przesyłki, przesyłka jeszcze nie nadana' => 'Parcel Registered, waiting for pickup.',
         'Przyjęcie przesyłki w oddziale DPD ' => 'Parcel has been picked up in office',
@@ -19,6 +15,12 @@ class Api extends CI_Controller {
         'Przesyłka doręczona ' => 'Parcel delivered',
         'Przesyłka odebrana przez Kuriera' => 'Parcel has been picked up by our agent'
     );
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Tracking');
+        $this->load->model('Log');
+    }
 
     private function checkParcelDelivery($trackingLog, $tracking) {
         if ($trackingLog->description == 'Przesyłka doręczona ') {
@@ -26,33 +28,22 @@ class Api extends CI_Controller {
         }
     }
 
-    private function setTrackingDataArray($array, $dataObject, $idTracking) {
-        array_push($array, array(
+    private function setTrackingDataArray($dataObject, $idTracking) {
+        array_push($this->data, array(
             'Date' => $dataObject->eventTime,
             'Description' => $dataObject->description,
             'Tracking_idTracking' => $idTracking
                 )
         );
-        return $array;
     }
 
     public function updateTrackingLog() {
 
-
-
-        /* $trackingNumbers = $this->Tracking->getTrackings();
-          $connector = new SoapConnectorPl();
-          $resultObject = $connector->checkTrackingNumberPl(13439300067559);
-          var_dump($resultObject);
-          echo(gettype($resultObject->return->eventsList));
-          die();
-         */
         $trackingNumbers = $this->Tracking->getTrackings();
         $connector = new SoapConnectorPl();
         foreach ($trackingNumbers as $tracking) {
             $data = [];
             $resultObject = $connector->checkTrackingNumberPl($tracking->realTracking);
-            //$resultObject = $connector->checkTrackingNumberPl(13439300067559);
             if (!isset($resultObject->return->eventsList)) {
                 continue;
             }
@@ -61,16 +52,16 @@ class Api extends CI_Controller {
             if (gettype($result) === "object") {
                 $this->checkParcelDelivery($result, $tracking->realTracking);
                 $result->description = $this->descriptionLibrary[$result->description];
-                $data = $this->setTrackingDataArray($data, $result, $tracking->idTracking);
+                $this->setTrackingDataArray($result, $tracking->idTracking);
             } else {
                 foreach ($result as $singleResult) {
                     $this->checkParcelDelivery($singleResult, $tracking->realTracking);
                     $singleResult->description = $this->descriptionLibrary[$singleResult->description];
-                    $data = $this->setTrackingDataArray($data, $singleResult, $tracking->idTracking);
+                    $this->setTrackingDataArray($singleResult, $tracking->idTracking);
                 }
             }
 
-            foreach ($data as $singleLog) {
+            foreach ($this->data as $singleLog) {
                 $this->Log->insertTracking($singleLog);
             }
         }
